aca9c57dc20187f92e8c696b17eedf3cde0362a3
supersfel <jmg081005@naver.com>
2022-04-17 13:54:13 +0900

    주석

--- 직접구현/02_multiple_linear_regression_2017142037.py
+++ 직접구현/02_multiple_linear_regression_2017142037.py
@@ -4,92 +4,92 @@ import matplotlib.pyplot as plt
 import os
 current_path = os.path.dirname(os.path.abspath(__file__))
 raw_data = pd.read_csv('multiple_linear_regression_data.csv',encoding='utf-8', engine = 'python')
-X = raw_data[['height','weight']].to_numpy()
-X = np.c_[X,np.ones(80)]
-print(X)
+X = raw_data[['height','weight']].to_numpy()    # x0 , x1 데이터 받기
+X = np.c_[X,np.ones(80)]   # X데이터 있는 배열 뒤에 1 붙이기 ( 나중에 w구해서 계산하기 위함 )
 Y = raw_data['label'].to_numpy()
-Y = Y.reshape(80,1)
+Y = Y.reshape(80,1)   # 행렬계산을 위해 맞춰줌
 
-
-#실습1
-height = raw_data['height'].to_numpy()
-weight = raw_data['weight'].to_numpy()
-age = raw_data['label'].to_numpy()
-N = len(height)
-
+#------------------------------------------------------------------
+#                          실습1
+#------------------------------------------------------------------
+height = raw_data['height'].to_numpy()  # X0데이터
+weight = raw_data['weight'].to_numpy()  # X1 데이터
+age = raw_data['label'].to_numpy()    #Y 데이터
+N = len(height)   # N = 80 ( 데이터 수 )
 
 fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
+ax = fig.add_subplot(projection='3d')   # 3차원 그래프를 그리기 위함
 ax.scatter(height,weight,age)
-#plt.show()
-
-#실습2
-analytic_W = np.linalg.pinv(X.T @ X) @ X.T @ Y
-height_data = np.linspace(55,190,1000)
-weight_data = np.linspace(10,100,1000)
+ax.set_xlabel('height')
+ax.set_ylabel('weight')
+ax.set_zlabel('age')
+#plt.show()   # 마지막에 한번에 출력하기위해 주석
+#------------------------------------------------------------------
+#                           실습2
+#------------------------------------------------------------------
+analytic_W = np.linalg.pinv(X.T @ X) @ X.T @ Y    # 해석해 구하는 식을 행렬계산으로 풀이
+height_data = np.linspace(55,190,1000)   # 55 ~ 190을 1000개로 나눈 배열
+weight_data = np.linspace(10,100,1000)   # 10 ~ 100을 1000개로 나눈 배열
 Height,Weight = np.meshgrid(height_data,weight_data)
-analytic_y = analytic_W[0]*Height + analytic_W[1]*Weight + analytic_W[2]
+analytic_y = analytic_W[0]*Height + analytic_W[1]*Weight + analytic_W[2]   # W0 * X0 + W1 * X1 + W2 식 구현
+print(analytic_W)
 
-fig = plt.figure()
+fig = plt.figure()   # 그래프 그리기
 ax = fig.add_subplot(projection='3d')
 ax.scatter(height,weight,age)
 ax.plot_surface(Height,Weight,analytic_y,cmap='plasma')
 ax.set_xlabel('height')
 ax.set_ylabel('weight')
 ax.set_zlabel('age')
-ax.set_title('Analytic Solution')
-#plt.show()
+ax.set_title('Analytic Solution')   #해석해로 구한 평면
+#plt.show()  # 마지막에 한번에 출력하기위해 주석
 
-#실습3
-
-MSE_Analytic_Solution = np.mean( (X @ analytic_W - Y) ** 2 )
+#------------------------------------------------------------------
+#                           실습3
+#------------------------------------------------------------------
+MSE_Analytic_Solution = np.mean( (X @ analytic_W - Y) ** 2 )  # 해석해로 구한 식의 MSE값을 행렬로 계산
 print('----------------')
 print('실습3')
 print('MSE_Analytic_Solution :',MSE_Analytic_Solution)
 print('----------------')
 
-
-
-#실습4
+#------------------------------------------------------------------
+#                           실습4
+#------------------------------------------------------------------
 print('실습4')
-def Gradient_Descent(lr,Gradient_W,epoch):
-    MSE_Gradient_Descent = np.mean((X @ Gradient_W - age) ** 2)
-    print('before MSE_Gradient_Descent :', MSE_Gradient_Descent)
+def Gradient_Descent(lr,Gradient_W,epoch):    #경사하강법 함수 learning rate , epoch 와 W들을 인자로 받음
+    MSE_Gradient_Descent = np.mean((X @ Gradient_W - age) ** 2)      #경사하강법으로 구한 해석해의 MSE
+    print('before MSE_Gradient_Descent :', MSE_Gradient_Descent)     #학습 시키기 전의 MSE
     print('')
 
-    for i in range(epoch):
-        y_pred = X @ Gradient_W
-        error = y_pred - age
+    for i in range(epoch):   # epoch만큼 반복
+        y_pred = X @ Gradient_W   # 바뀐 W들로 구한 예측값
+        error = y_pred - age      # 예측값과의 차이
 
-
-        W0_diff = np.mean((height * error))
+        W0_diff = np.mean((height * error))    # 각 W들의 기울기 구하기
         W1_diff = np.mean((weight * error).mean())
         W2_diff = np.mean(error)
 
-
-        Gradient_W[0] = Gradient_W[0] - lr * W0_diff
+        Gradient_W[0] = Gradient_W[0] - lr * W0_diff    # W들 값 갱신
         Gradient_W[1] = Gradient_W[1] - lr * W1_diff
         Gradient_W[2] = Gradient_W[2] - lr * W2_diff
 
-
-        MSE_Gradient_Descent = np.mean((X @ Gradient_W - age) ** 2)
-        if i%500 ==0:
+        if i%500 ==0:   # 500번 학습할때마다 W0,W1,W2값과 MSE값 출력
+            MSE_Gradient_Descent = np.mean((X @ Gradient_W - age) ** 2)  # 학습을 시켰으므로 MSE값 갱신
             print( "epoch",i, '====> W0 =',Gradient_W[0],'W1 =',Gradient_W[1],'W2 =',Gradient_W[2],'MSE =',MSE_Gradient_Descent)
 
-
     print('')
-    print('END MSE_Gradient_Descent :', MSE_Gradient_Descent)
+    print('END MSE_Gradient_Descent :', MSE_Gradient_Descent)   #학습 종료시 MSE출력
     return Gradient_W
 
-
-Gradient_W = Gradient_Descent(lr=0.00001,Gradient_W=[1,2,-5],epoch=10000)
+Gradient_W = Gradient_Descent(lr=0.00001,Gradient_W=[1,2,-5],epoch=10000)   #넣어준 값들로 학습시킨 후 Gradient_W에 저장
 
 print('----------------')
-print('W0 =',Gradient_W[0],'W1 =',Gradient_W[1],'W2 =',Gradient_W[2])
+print('W0 =',Gradient_W[0],'W1 =',Gradient_W[1],'W2 =',Gradient_W[2])  #학습 완료 후 W값들 출력
 print('----------------')
 
-gradient_y = Gradient_W[0]*Height + Gradient_W[1]*Weight + Gradient_W[2]
-fig = plt.figure()
+gradient_y = Gradient_W[0]*Height + Gradient_W[1]*Weight + Gradient_W[2]  #경사하강법으로 구한 W를 사용하여 구한 모델
+fig = plt.figure()   #모델 출력
 ax = fig.add_subplot(projection='3d')
 ax.scatter(height,weight,age)
 ax.plot_surface(Height,Weight,gradient_y,cmap='plasma')
@@ -97,5 +97,5 @@ ax.set_xlabel('height')
 ax.set_ylabel('weight')
 ax.set_zlabel('age')
 ax.set_title('Gradient Desent')
-#plt.show()
+plt.show()
 
--- 직접구현/03_Linear_Basis_2017142037.py
+++ 직접구현/03_Linear_Basis_2017142037.py
@@ -1,49 +1,41 @@
 import numpy as np
 import pandas as pd
-import math
 import matplotlib.pyplot as plt
-import os
-
-#실습5
+#------------------------------------------------------------------
+#                        실습5,6
+#------------------------------------------------------------------
 raw_data = pd.read_csv('linear_regression_data01.csv', names=['age', 'tall'])
 X = np.asarray(raw_data['age'].values.tolist())  # 나이 데이터(Xn)
 Y = np.asarray(raw_data['tall'].values.tolist())  # 키 데이터(Yn)
 
-N,Xmin,Xmax = len(X), min(X),max(X)
+N,Xmin,Xmax = len(X), min(X),max(X)   #계산시 반복되는 값들을 미리 저장
 
-def Gaussian_linear(K):
-    sigma = (Xmax - Xmin) / ( K - 1 )
-    U = sigma * np.arange(K) + Xmin
+def Gaussian_linear(K):    #가우스 함수를 이용한 선형 기저함수 회귀모델를 보기위한 함수 K개의 가우스 함수를 사용할지 인자로 받음
+    sigma = (Xmax - Xmin) / ( K - 1 )   #시그마 값 계산
+    U = sigma * np.arange(K) + Xmin     #k가 0~K-1까지 들어있는 Uk를 모음
     
-    Gaussian_func=[]
-    print(K,'sigma',sigma)
-    
+    Gaussian_func=[] #가우스 함수들을 저장할 배열 생성
     for Uk in U:
-        Gaussian_func.append( math.exp(1) ** (-0.5 * ( (X - Uk) / sigma )**2) )
+        Gaussian_func.append( np.exp(-0.5 * ( (X - Uk) / sigma )**2) )   #가우스 함수들을 저장
     
     Gaussian_func = np.array(Gaussian_func)
-    Gaussian_func = Gaussian_func.T
-    print(Gaussian_func)
-    Gaussian_func = np.c_[Gaussian_func,np.ones(N)]
-    
-   
+    Gaussian_func = Gaussian_func.T  # 저장된 값들이 행이아닌 열기준으로 함수가 저장되기에 전치행렬을 해줌
+    Gaussian_func = np.c_[Gaussian_func,np.ones(N)] #예측값 계산을 위해 뒤에 1인 배열을 붙여줌
 
-    Gaussian_W = np.linalg.pinv(Gaussian_func.T @ Gaussian_func) @ Gaussian_func.T @ Y
+    Gaussian_W = np.linalg.pinv(Gaussian_func.T @ Gaussian_func) @ Gaussian_func.T @ Y # 식에 의해서 구한 W값 계산
 
-    Gaussian_func_data = []
+    Gaussian_func_data = []   # 예측값들을 무작위로 보기위한 데이터들
     X_data = np.linspace(Xmin, Xmax, 1000)
-    
     for Uk in U:
-        Gaussian_func_data.append( math.exp(1) ** (-0.5 * ( (X_data - Uk) / sigma )**2) )
-    
+        Gaussian_func_data.append( np.exp(-0.5 * ( (X_data - Uk) / sigma )**2) )  #위와 같은작업 반복
 
-    Gaussian_func_data = np.array(Gaussian_func_data)
-    Gaussian_func_data = Gaussian_func_data.T
-    Gaussian_func_data = np.c_[Gaussian_func_data, np.ones(1000)]
+    Gaussian_func_data = np.array(Gaussian_func_data)  #위와 같은작업 반복
+    Gaussian_func_data = Gaussian_func_data.T  #위와 같은작업 반복
+    Gaussian_func_data = np.c_[Gaussian_func_data, np.ones(1000)]  #위와 같은작업 반복
 
-    MSE_Gaussian_Solution = np.mean((Gaussian_func @ Gaussian_W - Y) ** 2)
-    MSEs.append(MSE_Gaussian_Solution)
-    print('-------------------------------')
+    MSE_Gaussian_Solution = np.mean((Gaussian_func @ Gaussian_W - Y) ** 2)   #가우스 함수를 이용한 선형 기저함수 모델의 MSE계산
+    MSEs.append(MSE_Gaussian_Solution) #구한 MSE들을 저장
+    print('-------------------------------')    #구한 W들을 차례대로 출력
     print('K =',K )
     for idx in range(len(Gaussian_W)):
         print('w[',idx,'] =',Gaussian_W[idx],sep='')
@@ -53,27 +45,27 @@ def Gaussian_linear(K):
     Gaussian_Y = Gaussian_func_data @ Gaussian_W
     plt.grid(True, linestyle='--')
     plt.scatter(X, Y, color='green',label='original')  # 데이터 위치를 점으로 표시
-    plt.scatter(X_data, Gaussian_Y,s=3,label='k= '+str(K) + ', MSE =' + str(MSE_Gaussian_Solution))  # 데이터 위치를 점으로 표시
+    plt.scatter(X_data, Gaussian_Y,s=3,label='k= '+str(K) + ', MSE =' + str(MSE_Gaussian_Solution))  # 라벨에 K와 MSE를 표시
     plt.title('Traing '+str(K))
     plt.xlabel('age')
     plt.ylabel('tall')
     plt.legend()
 
-
 MSEs = [] # MSE 저장공간
-
-plt.subplot(221)
-Gaussian_linear(3)
+plt.subplot(221)  #한 화면 안에 그래프 4개를 그리기 위함
+Gaussian_linear(3)   #k=3 일때 가우스 함수를 이용한 선형 기저함수 모델
 plt.subplot(222)
-Gaussian_linear(5)
+Gaussian_linear(5)   #k=5 일때 가우스 함수를 이용한 선형 기저함수 모델
 plt.subplot(223)
-Gaussian_linear(8)
+Gaussian_linear(8)   #k=8 일때 가우스 함수를 이용한 선형 기저함수 모델
 plt.subplot(224)
-Gaussian_linear(10)
+Gaussian_linear(10)  #k=10 일때 가우스 함수를 이용한 선형 기저함수 모델
 plt.show()
-
+#------------------------------------------------------------------
+#                         실습 7
+#------------------------------------------------------------------
+# 강의자료와 유사하게 출력하기 위해 점을 찍고 점에서 수직으로 그래프를 그림
 plt.scatter([3,5,8,10], MSEs,label='MSE',color='dodgerblue')  # 데이터 위치를 점으로 표시
-
 plt.plot([3,3],[0,MSEs[0]],color='dodgerblue')
 plt.plot([5,5],[0,MSEs[1]],color='dodgerblue')
 plt.plot([8,8],[0,MSEs[2]],color='dodgerblue')
