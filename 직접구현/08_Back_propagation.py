0f53bb9ed67a33c05704318eb6dbd39170085bc7
supersfel <jmg081005@naver.com>
2022-05-30 23:25:26 +0900

    08

--- 직접구현/08_Back_propagation.py
+++ 직접구현/08_Back_propagation.py
@@ -1,7 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+
+
 class perceptron: #perceptron 클래스 구현
     def __init__(self,w):
         self.w = w
@@ -28,7 +34,7 @@ def one_hot_encoding(array,size):
     return np.array(lst)
 
 class Neural_Network: #2계층 신경망 구현
-    def __init__(self,hidden_layer_size,Input,Output,learning_rate):
+    def __init__(self,hidden_layer_size,Input,Output,learning_rate,Test_set):
         self.hidden_layer_size = hidden_layer_size
         self.Input_size = Input.shape[1]
         self.Output_size= Output.shape[1]
@@ -36,6 +42,7 @@ class Neural_Network: #2계층 신경망 구현
         self.Y = Output
         self.learning_rate = learning_rate
         self.Create_Weight_Matrix()
+        self.Test_set = Test_set
 
     def Create_Weight_Matrix(self): #Weight 를 만드는 함수
         self.W0 = np.random.rand(self.Input_size+1, self.hidden_layer_size)
@@ -60,8 +67,6 @@ class Neural_Network: #2계층 신경망 구현
             Y_pred = self.predict(self.X[i])
             Input = np.append(self.X[i], 1)
 
-            # print('before Back-propagation W0')
-            # print(self.W0)
             for j in range(self.Input_size + 1):  # 역전파 2단계
                 for k in range(self.hidden_layer_size):
                     Etotal_h_diff = 0
@@ -72,11 +77,7 @@ class Neural_Network: #2계층 신경망 구현
                     Etotal_w = Etotal_h_diff * h_z_diff * z_w_diff
 
                     self.W0[j][k] = self.W0[j][k] - lr * Etotal_w
-            # print('after Back-propagation W0')
-            # print(self.W0)
 
-            # print('before Back-propagation W1')
-            # print(self.W1)
             for j in range(self.hidden_layer_size+1): #역전파 1단계
                 for k in range(self.Output_size):
                     E_o_diff = -2 * ( self.Y[i][k] - Y_pred[k] )
@@ -85,14 +86,12 @@ class Neural_Network: #2계층 신경망 구현
                     Etotal_w = E_o_diff * o_z_diff * z_w_diff
 
                     self.W1[j][k] = self.W1[j][k] - lr * Etotal_w
-            # print('after Back-propagation W1')
-            # print(self.W1)
-            # print('===========================')
 
     def train(self,epoch):
         self.epoch = epoch
-
+        self.MSEs = []
         for i in range(epoch):
+
             data = np.concatenate([self.X,self.Y],1)  #셔플과정
             np.random.shuffle(data)
             self.X,none,self.Y = np.hsplit(data,(self.Input_size,self.Input_size))
@@ -100,12 +99,36 @@ class Neural_Network: #2계층 신경망 구현
             self.Back_propagation()
 
             if i % 100 ==0 :
-                tmp=[]
+
+                tmp_mse=[]
+                cnt = 0
                 for j in range(len(self.X)):
                     Y_pred = self.predict(self.X[j])
-                    tmp.append(np.mean((self.Y[j] - Y_pred)**2 ))
-                MSE = np.mean(tmp)
-                print(f'EPOCH {i} ===> MSE : {MSE}')
+                    tmp_mse.append(np.mean((self.Y[j] - Y_pred)**2 ))
+                    maxindex = np.argmax(self.predict(self.X[j]))
+                    tmp = np.array([0] * self.Output_size)
+                    tmp[maxindex] = 1
+                    if np.array_equal(tmp,self.Y[j]):
+                        cnt +=1
+                Accuracy = cnt / len(self.X)
+
+                test_X,none,test_Y = np.hsplit(Test_set,(self.Input_size,self.Input_size))
+                cnt = 0
+                for j in range(len(test_X)):
+                    maxindex = np.argmax(self.predict(test_X[j]))
+                    tmp = np.array([0] * self.Output_size)
+                    tmp[maxindex] = 1
+                    if np.array_equal(tmp,test_Y[j]):
+                        cnt +=1
+
+                test_Accuracy = cnt / len(test_X)
+
+                MSE = np.mean(tmp_mse)
+                self.MSEs.append(MSE)
+
+
+                print(f'EPOCH {i} ===> MSE : {MSE} , Accuracy : {Accuracy} , test_Accuracy : {test_Accuracy}')
+
 
 
 
@@ -120,11 +143,12 @@ Y = raw_data['y'].to_numpy() #y 데이터 받기
 Y = one_hot_encoding(Y,3)
 NN_data = np.concatenate([X,Y],1)
 
+np.random.shuffle(NN_data)
 Traning_set = NN_data[:int(0.7*NN_data_size)]
 Test_set = NN_data[int(0.7*NN_data_size):]
 X,none,Y = np.hsplit(Traning_set,(3,3))
 
-Network = Neural_Network(hidden_layer_size=4,Input=X,Output=Y,learning_rate=0.0003)
+Network = Neural_Network(hidden_layer_size=10,Input=X,Output=Y,learning_rate=0.0001,Test_set=Test_set)
 Network.Check_Input_Output_size()
 
-Network.train(100000)
+Network.train(10000)
